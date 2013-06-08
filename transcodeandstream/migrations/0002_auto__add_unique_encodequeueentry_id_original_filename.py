54cff5ceac91708d7e07bb124cb2428dde6bd208
Lorenzo Masini <rugginoso@develer.com>
2013-06-08 16:44:28 +0200

    Added constraints to EncodeQueueEntry model.

---
+++ transcodeandstream/migrations/0002_auto__add_unique_encodequeueentry_id_original_filename.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding unique constraint on 'EncodeQueueEntry', fields ['id', 'original_filename']
+        db.create_unique(u'transcodeandstream_encodequeueentry', ['id', 'original_filename'])
+
+
+    def backwards(self, orm):
+        # Removing unique constraint on 'EncodeQueueEntry', fields ['id', 'original_filename']
+        db.delete_unique(u'transcodeandstream_encodequeueentry', ['id', 'original_filename'])
+
+
+    models = {
+        u'transcodeandstream.encodequeueentry': {
+            'Meta': {'ordering': "('created_at',)", 'unique_together': "(('id', 'original_filename'),)", 'object_name': 'EncodeQueueEntry'},
+            'created_at': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'id': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '10', 'primary_key': 'True'}),
+            'original_filename': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'working_on': ('django.db.models.fields.BooleanField', [], {'default': 'False'})
+        }
+    }
+
+    complete_apps = ['transcodeandstream']
--- transcodeandstream/models.py
+++ transcodeandstream/models.py
@@ -1,10 +1,11 @@
 from django.db import models
 
 class EncodeQueueEntry(models.Model):
-	id = models.CharField(max_length=10, primary_key=True)
+	id = models.CharField(max_length=10, primary_key=True, unique=True)
 	original_filename = models.CharField(max_length=255)
 	created_at = models.DateTimeField(auto_now_add=True)
 	working_on = models.BooleanField(default=False)
 
 	class Meta:
 		ordering = ('created_at',)
+		unique_together = ('id', 'original_filename')
